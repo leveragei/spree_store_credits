ff6034cdaff4e4db3ba45a2d622a2b8900f80a68
Nima Shariatian <nima@onevio.org>
2014-08-22 15:00:00 -0400

    updates: spec folder, installs rspec 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,58 +1,26 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
-require 'simplecov' if ENV["COVERAGE"]
-
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-
-require 'rspec/rails'
-require 'factory_girl'
-require 'spree/testing_support/url_helpers'
-require 'database_cleaner'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
-
-# Requires factories defined in spree_core
-require 'spree/testing_support/factories'
-
-# include local factories
-Dir["#{File.dirname(__FILE__)}/factories/**/*.rb"].each { |f| require File.expand_path(f)}
-
-require 'spree/testing_support/controller_requests'
-require 'spree/testing_support/authorization_helpers'
-
-require 'ffaker'
-
 RSpec.configure do |config|
-  config.color = true
-  config.mock_with :rspec
 
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  config.mock_with :rspec
+  config.color = true
+  config.formatter = :documentation
+
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, comment the following line or assign false
-  # instead of true.
+  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
+  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
+  # to setup a test will be unavailable to the browser, which runs under a separate server instance.
   config.use_transactional_fixtures = false
-  config.before(:each) do
-    if example.metadata[:js]
-      DatabaseCleaner.strategy = :truncation
-    else
-      DatabaseCleaner.strategy = :transaction
-    end
-  end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+  config.infer_spec_type_from_file_location!
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-  config.include FactoryGirl::Syntax::Methods
-  config.include Spree::TestingSupport::UrlHelpers
-  config.include Spree::TestingSupport::ControllerRequests, :type => :controller
-  config.include Rack::Test::Methods, :type => :feature
-  config.include Capybara::DSL
+  config.fail_fast = true
+  config.order = "random"
 end
